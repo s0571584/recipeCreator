58155ce24787ce794c6fb8fd5a731a15e54812aa
s0571584 <s0571584@htw-berlin.de>
2023-04-24 22:29:07 +0200

    Initial commit

---
+++ webtech/src/main/java/htwberlin/webtech/Welcome.java
@@ -0,0 +1,15 @@
+package htwberlin.webtech;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Welcome {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Hello World";
+    }
+}
