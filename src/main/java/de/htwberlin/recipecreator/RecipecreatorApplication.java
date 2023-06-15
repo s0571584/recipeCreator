d59e0b4d4919b49cce3fa9b2b310f282866f127d
s0571584 <s0571584@htw-berlin.de>
2023-06-15 05:41:44 +0200

    frontend connection established

--- src/main/java/de/htwberlin/recipecreator/RecipecreatorApplication.java
+++ src/main/java/de/htwberlin/recipecreator/RecipecreatorApplication.java
@@ -1,13 +1,25 @@
 package de.htwberlin.recipecreator;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class RecipecreatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipecreatorApplication.class, args);
+	}
+
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedMethods("*")
+					.allowedOrigins("*");
+		}
 	}
 
 }
